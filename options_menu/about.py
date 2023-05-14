8b0f49dd83a5c3486de74eb4373ec09d8f81310d
Aleksei Maksimov <gumbeat@gmail.com>
2023-05-15 00:37:41 +0530

    1. Added About app section 2. Translated About app

--- options_menu/about.py
+++ options_menu/about.py
@@ -4,11 +4,9 @@ import streamlit as st
 
 
 def about_page():
-    try:
-        if translation.lang == 'ru':
-            with open(os.getcwd() + '/options_menu/about_ru.txt', 'r', encoding='utf-8') as file:
-                st.markdown(file.read())
-    except AttributeError:
+    if st.session_state['language'] == 'Русский':
+        with open(os.getcwd() + '/options_menu/about_ru.txt', 'r', encoding='utf-8') as file:
+            st.markdown(file.read())
+    else:
         with open(os.getcwd() + '/options_menu/about_en.txt', 'r', encoding='utf-8') as file:
             st.markdown(file.read())
-    return about_page
