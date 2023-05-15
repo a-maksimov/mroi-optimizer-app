b636193a7a47970beaa80c54804260e41fd4928d
Aleksei Maksimov <gumbeat@gmail.com>
2023-05-15 17:49:37 +0530

    Fixed a bug with language switch when on About page.

--- options_menu/about.py
+++ options_menu/about.py
@@ -3,7 +3,8 @@ import streamlit as st
 
 
 def about_page():
-    if st.session_state['language'] == 'Русский':
+    # if Russian
+    if st.session_state['language'] == 'Русский' or st.session_state['language_switch_counter'] % 2 == 0:
         with open(os.getcwd() + '/options_menu/about_ru.txt', 'r', encoding='utf-8') as file:
             st.markdown(file.read())
     else:
