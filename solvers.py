Bugs & fixes. 1. Reset simulation and optimization after change of date range and periodicity. 2. Preserve date range and periodicity after language switch. 3. Fixed bug with boundaries calculation. 4. Fixed bug with menu translation on Planning and Optimization pages.

solvers.py
@@ -1,5 +1,5 @@
 from scipy.optimize import minimize
-# import nlopt
+import nlopt
 import numpy as np
 from translations import _
 