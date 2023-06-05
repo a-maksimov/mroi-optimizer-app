1134c3844b2f3bc570c88d744fd1333230963fb2
diana.davydova <diana.davydova@ax-team.com>
2023-06-05 11:25:29 +0300

    new colors for plots

--- options_menu/specification_tabs/tab_plotting.py
+++ options_menu/specification_tabs/tab_plotting.py
@@ -13,6 +13,23 @@ def spec_plotting_tab(dataframe):
     granularity_levels = [_(level) for level in read_data.granularity_levels]
     numeric_variables = [_(variable) for variable in read_data.numeric_variables if
                          _(variable) in dataframe.columns]
+    
+    base_colors_timeseries=['#eb6e21','#f7db05','#d62b2b','#1e3ac7','#13bd21','#7308c4','#d936c6']
+
+    colors_contribution=['#F7A15B','#FFEC5E','#F76F6F','#8554F7','#4AF77B', '#C64AF7', '#F763F7']
+    colors_spend=['#C25F0E', '#EBD110', '#C21D1D', '#4008C2', '#00C237', '#8B00C2', '#C213C2']
+    colors_revenue=['#F57811','#F5F231','#F52525','#5109F4','#00F545', '#B000F5', '#F518F5']
+
+    spend_colors_mroi_list=[]
+    revenue_colors_mroi_list=[] 
+    contribution_colors_mroi_list=[]
+    mroi_color_list=[]
+
+    granularity_colors ={}
+    contribution_colors ={}
+    spend_colors ={}
+    revenue_colors ={}
+    mroi_colors={}
 
     # check if user has selected any granularity in sidebar
     # iterate backwards through the granularity levels and use the first one that is not empty
@@ -49,6 +66,12 @@ def spec_plotting_tab(dataframe):
         contribution_dim = '%'
         pct = '_pct'
 
+    for i in range(len(granularity_to_plot)):
+        granularity_colors[granularity_to_plot[i]] = base_colors_timeseries[i]
+        contribution_colors[granularity_to_plot[i]] = colors_contribution[i]
+        spend_colors[granularity_to_plot[i]] = colors_spend[i]
+        revenue_colors[granularity_to_plot[i]] = colors_revenue[i]
+
     # Timeseries plot
     fig = go.Figure()
     for numeric_variable in numeric_variables_to_plot:
@@ -62,13 +85,15 @@ def spec_plotting_tab(dataframe):
                     fig.add_trace(go.Bar(x=timeseries.index,
                                          y=timeseries[timeseries[level] == granularity][numeric_variable],
                                          name=f'{numeric_variable}, {granularity}',
-                                         opacity=0.6,
+                                         marker_color=spend_colors[granularity],
+                                         opacity=0.6
                                          )
                                   )
                 elif _('Revenue').lower() in numeric_variable.lower():
                     fig.add_trace(go.Scatter(x=timeseries.index,
                                              y=timeseries[timeseries[level] == granularity][numeric_variable],
                                              name=f'{numeric_variable}, {granularity}',
+                                             marker_color=revenue_colors[granularity],
                                              mode='markers+lines'
                                              )
                                   )
@@ -77,6 +102,7 @@ def spec_plotting_tab(dataframe):
                                              y=timeseries[timeseries[level] == granularity][numeric_variable],
                                              name=f'{numeric_variable}, {granularity}',
                                              mode='markers+lines',
+                                             marker_color=contribution_colors[granularity],
                                              yaxis='y2'
                                              )
                                   )
@@ -119,13 +145,31 @@ def spec_plotting_tab(dataframe):
     fig = go.Figure()
     # calculate mroi data
     data = mroi_plot(dataframe, level, numeric_variables, percents=percents)
+    labels_for_colors=[]
     if data is not None:
+        for i in range(len(data.index)):
+            labels_for_colors.append(data.index[i])
+
+        for label in labels_for_colors:
+            contribution_colors_mroi_list.append(contribution_colors[label])
+            spend_colors_mroi_list.append(spend_colors[label])
+            revenue_colors_mroi_list.append(revenue_colors[label])
+        mroi_color_list.append(spend_colors_mroi_list)
+        mroi_color_list.append(revenue_colors_mroi_list)
+        mroi_color_list.append(contribution_colors_mroi_list)
+            
+        k=0
+        for variable in sorted(set(numeric_variables_to_plot).intersection(numeric_variables)):
+            mroi_colors[variable] = mroi_color_list[k]
+            k=k+1
+
         for variable in sorted(set(numeric_variables_to_plot).intersection(numeric_variables)):
             fig.add_trace(go.Bar(x=data.index,
                                  y=data[variable + pct],
                                  name=data[variable].name,
                                  opacity=0.6,
                                  xperiodalignment='middle',
+                                 marker_color=mroi_colors[variable]
                                  )
                           )
         mroi_data = data[_('Revenue Calculated')] / data[_('Spend')]
@@ -136,6 +180,7 @@ def spec_plotting_tab(dataframe):
                                  mode='markers',
                                  marker=dict(
                                      size=10,
+                                     color=mroi_colors[variable]
                                  ),
                                  yaxis='y2',
                                  text=mroi_data,  # Include Y values in marker text
